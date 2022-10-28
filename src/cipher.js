8c805d4ac8c262013e9b1def2dc1a2344cf2333c
FerRomero1990 <aidafernanda1990@hotmail.com>
2022-10-28 15:41:24 -0500

    cambios cipher js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,10 @@
 const cipher = {
-  // ...
+  encode:function(offset, string){
+    //codigo para la funcion
+  },
+  decode:function(offset, string){
+    //codigo para la funcion
+  }
 };
 
 export default cipher;
