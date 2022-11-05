2f3f6dea84e2aece425f4cf8e933a8453af7121d
FerRomero1990 <aidafernanda1990@hotmail.com>
2022-11-04 21:07:07 -0500

    cifrado resuelto

--- src/cipher.js
+++ src/cipher.js
@@ -1,10 +1,39 @@
 const cipher = {
-  encode:function(offset, string){
-    //codigo para la funcion
-  },
-  decode:function(offset, string){
-    //codigo para la funcion
-  }
-};
-
+    encode:function (texto, desplazamiento) {
+        let resultado = ""
+        const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+        desplazamiento = (desplazamiento % 26 + 26) % 26;
+    
+        if (texto) {
+            for (let i = 0; i < texto.length; i++) {
+                if (abc.indexOf(texto[i]) != -1) {
+                    let posicion = ((abc.indexOf(texto[i]) + desplazamiento) % 26);
+                    resultado += abc[posicion];
+                }
+                else{
+                    resultado += texto[i];}
+            }
+        }
+        return resultado;
+    },
+    decode:function (texto, desplazamiento) {
+        let resultado = ""
+        const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+        desplazamiento = (desplazamiento % 26 - 26) % 26;
+    
+        if (texto) {
+            for (let i = 0; i < texto.length; i++) {
+                if (abc.indexOf(texto[i]) != -1) {
+                    let posicion = ((abc.indexOf(texto[i]) - desplazamiento) % 26);
+                    resultado += abc[posicion];
+                }
+                else{
+                    resultado += texto[i];}
+            }
+        }
+        return resultado;
+    },
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import cipher from './cipher.js';
 
-cipher.decode()
+document.getElementById("descubrimientoACifrar").addEventListener("keyup", function () {
+    this.value = this.value.toUpperCase();
+});
+document.getElementById("cifrar").addEventListener("click", function () {
+    let texto = document.getElementById("descubrimientoACifrar").value;
+    let desplazamiento = document.getElementById("cifrasADesplazar").value;
+    document.getElementById("descubrimientoCifrado").value = cipher.encode(texto, desplazamiento);
+});
+document.getElementById("descifrar").addEventListener("click", function () {
+    let texto = document.getElementById("descubrimientoACifrar").value;
+    let desplazamiento = document.getElementById("cifrasADesplazar").value;
+    console.log(cipher.decode(texto, desplazamiento))
+    document.getElementById("descubrimientoCifrado").value = cipher.decode(texto, desplazamiento);
+});
+
+
+
+
+/*function cifrar2(texto, desplazamiento) {
+    if (!texto)
+        return "";
+    const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    desplazamiento = (desplazamiento % 26 + 26) % 26
+    return texto.replace(/[A - Z]/ig, c => letras[(letras.indexOf(c) + desplazamiento) % 26]);
+}*/
+
+
